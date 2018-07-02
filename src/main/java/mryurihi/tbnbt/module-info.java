d02e5cf8cd29d82276873985cced0fe8239567a2
MrYurihi <mryurihi2003@gmail.com>
2018-07-02 10:51:00 -0400

    *nope*

--- src/main/java/mryurihi/tbnbt/module-info.java
+++
@@ -1,7 +0,0 @@
-open module mryurihi.tbnbt {
-	exports mryurihi.tbnbt.adapter;
-	exports mryurihi.tbnbt.tag;
-	exports mryurihi.tbnbt.annotations;
-	exports mryurihi.tbnbt;
-	exports mryurihi.tbnbt.stream;
-}
