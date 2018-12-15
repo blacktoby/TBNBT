1485452caf79e48f48b9258a244c38a810b2ce0e
MrYurihi <mryurihi2003@gmail.com>
2018-12-15 13:58:54 -0500

    Change package for files

---
+++ src/main/java/com/github/mryurihi/tbnbt/tag/NBTTagList.java
@@ -0,0 +1,138 @@
+/*
+MIT License
+
+Copyright (c) 2017 MrYurihi Redstone
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.github.mryurihi.tbnbt.tag;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.mryurihi.tbnbt.TagType;
+
+public class NBTTagList extends NBTTag {
+	
+	private List<NBTTag> value;
+	
+	private TagType typeId;
+	
+	public NBTTagList(List<NBTTag> value, TagType typeId) {
+		this.value = new ArrayList<>();
+		if(value != null) for(NBTTag tag: value) {
+			this.value.add(tag);
+		}
+		this.typeId = typeId;
+	}
+	
+	public NBTTagList(TagType typeId) {
+		value = new ArrayList<>();
+		this.typeId = typeId;
+	}
+	
+	public NBTTagList(List<NBTTag> value) {
+		this.value = value;
+		if(value != null && ! value.isEmpty()) this.typeId = value.get(0).getTagType();
+		else throw new IllegalArgumentException("Cannot get tag type of list");
+	}
+	
+	NBTTagList() {}
+	
+	public List<NBTTag> getValue() {
+		return value;
+	}
+	
+	public NBTTag get(int index) {
+		return value.get(index);
+	}
+	
+	public TagType getTypeId() {
+		return typeId;
+	}
+	
+	public void setValue(List<NBTTag> value) {
+		this.value = value;
+	}
+	
+	public NBTTagList setTypeId(TagType typeId) {
+		this.typeId = typeId;
+		return this;
+	}
+	
+	public NBTTagList add(NBTTag tag) {
+		if(tag.getTagType() == typeId) value.add(tag);
+		else throw new IllegalArgumentException(tag.getClass().getName() + " is not the type " + typeId);
+		return this;
+	}
+	
+	public NBTTagList add(int index, NBTTag tag) {
+		if(tag.getTagType() == typeId) value.add(index, tag);
+		else throw new IllegalArgumentException(tag.getClass().getName() + " is not the type " + typeId);
+		return this;
+	}
+	
+	public NBTTagList remove(int index) {
+		value.remove(index);
+		return this;
+	}
+	
+	public NBTTagList set(int index, NBTTag tag) {
+		if(typeId.equals(TagType.END)) typeId = tag.getTagType();
+		if(tag.getTagType() == typeId) value.set(index, tag);
+		else throw new IllegalArgumentException(tag.getClass().getName() + " is not the type " + typeId);
+		return this;
+	}
+
+	@Override
+	public void writePayloadBytes(DataOutputStream out) throws IOException {
+		out.writeByte(typeId.getId());
+		out.writeInt(value.size());
+		for(NBTTag t: value) t.writePayloadBytes(out);
+	}
+	
+	@Override
+	public NBTTag readPayloadBytes(DataInputStream in) throws IOException {
+		value = new ArrayList<>();
+		byte id = in.readByte();
+		int length = in.readInt();
+		typeId = TagType.getTypeById(id);
+		value = new ArrayList<>();
+		for(int i = 0; i < length; i++) value.add(NBTTag.newTagByType(typeId, in));
+		return this;
+	}
+
+	@Override
+	public TagType getTagType() {
+		return TagType.LIST;
+	}
+	
+	@Override
+	public String toString() {
+		return value.toString();
+	}
+	
+	@Override
+	protected boolean equalsTag(NBTTag tag) {
+		return tag.getTagType().equals(TagType.LIST) && tag.getAsTagList().getValue().equals(value);
+	}
+}
---
+++ src/test/java/com/github/mryurihi/tbnbt/stream/NBTInputStreamTest.java
@@ -0,0 +1,186 @@
+/*
+MIT License
+
+Copyright (c) 2017 MrYurihi Redstone
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package com.github.mryurihi.tbnbt.stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.github.mryurihi.tbnbt.TagType;
+import com.github.mryurihi.tbnbt.adapter.AdapterRegistry;
+import com.github.mryurihi.tbnbt.adapter.NBTAdapter;
+import com.github.mryurihi.tbnbt.adapter.NBTParseException;
+import com.github.mryurihi.tbnbt.adapter.TypeWrapper;
+import com.github.mryurihi.tbnbt.stream.NBTInputStream;
+import com.github.mryurihi.tbnbt.tag.NBTTagString;
+
+class NBTInputStreamTest {
+	
+	NBTInputStream in;
+	
+	@AfterEach
+	void afterEach() throws IOException {
+		in.close();
+	}
+	
+	@Test
+	void testReadTagBoolean() throws IOException {
+		in = new NBTInputStream(getClass().getClassLoader().getResourceAsStream("data/nbt/namedstring.nbt"), false);
+		assertEquals(new NBTTagString("MrYurihi"), in.readTag());
+	}
+	
+	@Test
+	void testReadTag() throws IOException {
+		in = new NBTInputStream(getClass().getClassLoader().getResourceAsStream("data/nbt/namedstring.nbt"), false);
+		assertEquals(new NBTTagString("MrYurihi"), in.readTag());
+	}
+	
+	@Test
+	void testReadToType() throws IOException, NBTParseException {
+		in = new NBTInputStream(getClass().getClassLoader().getResourceAsStream("data/nbt/serializetest1.nbt"), false);
+		TestDataHolder expt = new TestDataHolder();
+		expt.int1 = 0x00000F00;
+		expt.string1 = "yay";
+		assertEquals(expt, in.readToType(TypeWrapper.of(TestDataHolder.class)));
+	}
+	
+	@Test
+	void testClose() throws IOException {
+		ProxyInputStream proxy = new ProxyInputStream();
+		
+		in = new NBTInputStream(proxy, false);
+		in.close();
+		
+		assertTrue(proxy.closed);
+	}
+	
+	static class ProxyInputStream extends InputStream {
+		public boolean closed = false;
+		
+		@Override
+		public int read() throws IOException {
+			return 0;
+		}
+		
+		@Override
+		public void close() throws IOException {
+			closed = true;
+		}
+	}
+	
+	static class TestDataHolder {
+		String string1;
+		int int1;
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof TestDataHolder) {
+				TestDataHolder obj2 = (TestDataHolder) obj;
+				return string1.equals(obj2.string1) && int1 == obj2.int1;
+			}
+			return false;
+		}
+	}
+	
+	@Test
+	void testReadToTypeWithAdapterRegistry() throws IOException, NBTParseException {
+		AdapterRegistry reg = new AdapterRegistry.Builder()
+			.addAdapter(TestDataHolder2.CustomData.class, new TestDataHolder2.CustomDataAdapter())
+			.create();
+		
+		TestDataHolder2 data = new TestDataHolder2();
+		data.data1 = new TestDataHolder2.CustomData(0x05, 0x21);
+		data.data2 = new TestDataHolder2.CustomData(0x01, 0x20);
+		
+		in = new NBTInputStream(getClass().getClassLoader().getResourceAsStream("data/nbt/customdata.nbt"), false);
+		assertEquals(data, in.readToType(TypeWrapper.of(TestDataHolder2.class), reg));
+	}
+	
+	static class TestDataHolder2 {
+		
+		CustomData data1;
+		CustomData data2;
+		
+		static class CustomData {
+			byte x, y;
+			
+			public CustomData(int x, int y) {
+				this.x = (byte) x;
+				this.y = (byte) y;
+			}
+			
+			@Override
+			public boolean equals(Object obj) {
+				if(obj instanceof CustomData) {
+					CustomData arg1 = (CustomData) obj;
+					return arg1.x == x && arg1.y == y;
+				}
+				return false;
+			}
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(obj instanceof TestDataHolder2) {
+				TestDataHolder2 arg1 = (TestDataHolder2) obj;
+				return arg1.data1.equals(data1) && arg1.data2.equals(data2);
+			}
+			return false;
+		}
+		
+		static class CustomDataAdapter extends NBTAdapter<CustomData> {
+			
+			@Override
+			public CustomData fromNBT(TagType id, DataInputStream payload, TypeWrapper<?> type, AdapterRegistry registry)
+					throws NBTParseException {
+				CustomData out = new CustomData(0, 0);
+				Byte[] data = registry.fromByteArray(payload);
+				out.x = data[0];
+				out.y = data[1];
+				return out;
+			}
+			
+			@Override
+			public void toNBT(DataOutputStream out, Object object, TypeWrapper<?> type, AdapterRegistry registry)
+					throws NBTParseException {
+				CustomData data = (CustomData) object;
+				registry.writeByteArray(out, new Byte[] {
+						data.x, data.y
+				});
+			}
+			
+			@Override
+			public TagType getId() {
+				return TagType.BYTE_ARRAY;
+			}
+			
+		}
+	}
+}
